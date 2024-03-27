97c0dc83f9202e195274ae494c2ab33b03aed3d0
SkySlark9929 <skyslark9929@gmail.com>
2024-03-27 08:48:12 +0300

    Allow user to change grid size through button
    Grid creation funcitonality was moved to
    createGrid function, which is invoked by the
    .change_size button
    
    The grid now consists of rows, populated with pixe
    ls, which allows for dynamic flex pixel size
     Changes to be committed:
    	modified:   index.html
    	modified:   source.js
    	modified:   style.css

--- source.js
+++ source.js
@@ -1,5 +1,9 @@
 let pixel;
+let row;
+let gridSide;
 let etchBoardContainer = document.querySelector('#etch_container');
+let changeSizeButton = document.querySelector('#change_size');
+changeSizeButton.addEventListener('click', createGrid);
 
 // Checks whether the mouse is currently down
 let mouseDown = 0;
@@ -10,12 +14,31 @@ document.body.onmouseup = function() {
   --mouseDown;
 }
 
-for (let i = 1; i < 10000; i++){
-    pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    pixel.addEventListener('mouseover', (e) => fillPixel(e));
+// It seems like we have two ways of accomplishing stuff, either through flex-basis\
+// or through container div for each row. For now we will gow with second option.
 
-    etchBoardContainer.appendChild(pixel);
+function createGrid(){
+  gridSide = prompt('Enter the number of pixels per sketch side. (No more than 100)');
+
+  if (gridSide > 100){
+    gridSide = 100;
+    alert('Error: The number of pixels exceeds 100! Set to 100 instead.')
+  };
+
+  // generate rows
+  for (let i = 1; i < gridSide; i++){
+      row = document.createElement('div');
+      row.classList.add('row');
+      etchBoardContainer.appendChild(row);
+
+      // generate pixels inside of rows
+      for (let _ = 1; _ < gridSide; _++){
+        pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.addEventListener('mouseover', (e) => fillPixel(e));
+        row.appendChild(pixel);
+      };
+  };
 };
 
 function fillPixel(e){
@@ -23,3 +46,7 @@ function fillPixel(e){
         e.target.classList.add('black_pixel');
     };
 }
+
+function removeGrid(){
+
+};
